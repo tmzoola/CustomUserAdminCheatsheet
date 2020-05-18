dde9d76c416ee7a371a57f8e5f5d94b15f671514
tmzoola <murodtokh1997@gmail.com>
2020-05-18 13:01:17 +0300

    First Commit

--- account/views.py
+++ account/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegistrationForm,AccountAuthenticate
+from django.contrib.auth import login, authenticate,logout
 
-# Create your views here.
+def registration_view(request):
+    context = {}
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            account = authenticate(email=email, password=raw_password)
+            login(request,account)
+            return redirect('home')
+        else:
+            context['registration_form'] = form
+    else:
+        form = RegistrationForm()
+        context['registration_form'] = form
+    return render(request, 'account/register.html',context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+def login_view(request):
+    context = {}
+    user = request.user
+    if user.is_authenticated:
+        return redirect('home')
+    if request.POST:
+        form = AccountAuthenticate(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+            if user:
+                login(request,user)
+                return redirect('home')
+    else:
+        form = AccountAuthenticate()
+    context['login_form'] = form
+    return render(request, 'account/login.html', context)
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from account.views import registration_view,logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('personal.urls')),
+    path('register/',registration_view, name='register'),
+    path('logout/',logout_view, name='logout'),
 ]
